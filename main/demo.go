43fe4c8e2c5484451a11eb8afcd805edeee07e50
流羽 <lycpan@foxmail.com>
2022-11-13 11:46:17 +0800

    fix: demo

--- main/demo.go
+++ main/demo.go
@@ -5,10 +5,7 @@ import (
 )
 
 func main() {
-	var a float64 = 0.1;
-	var b float64 = 0.2;
-	fmt.Printf("%f", a + b );
-	fmt.Printf("%f", .1 + .2);
-	fmt.Println( a + b );
-	fmt.Println( .1 + .2 );
+	var a int 
+	fmt.Scanln(&a)
+	fmt.Println(a)
 }
